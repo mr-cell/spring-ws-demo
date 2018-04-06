36fb5834acbc3071bda4fd01fca889bf52eb8f02
marcel.schally <marcel.schally@gmail.com>
2018-04-06 19:53:16 +0200

    initial commit

---
+++ src/main/java/mr/cell/springwsdemo/SpringWsDemoApplication.java
@@ -0,0 +1,12 @@
+package mr.cell.springwsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWsDemoApplication.class, args);
+	}
+}
